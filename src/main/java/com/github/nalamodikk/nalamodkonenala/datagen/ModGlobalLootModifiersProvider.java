0d3339c22c990ce4deaac3e81a5e4fb87e7166ba
BuildTools <unconfigured@null.spigotmc.org>
2024-07-28 22:08:23 +0800

    更新

---
+++ src/main/java/com/github/nalamodikk/nalamodkonenala/datagen/ModGlobalLootModifiersProvider.java
@@ -0,0 +1,34 @@
+package com.github.nalamodikk.nalamodkonenala.datagen;
+
+import com.github.nalamodikk.nalamodkonenala.Konenalamod;
+import com.github.nalamodikk.nalamodkonenala.loot.AddItemModifier;
+import com.github.nalamodikk.nalamodkonenala.item.ModItems;
+import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.storage.loot.predicates.LootItemBlockStatePropertyCondition;
+import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
+import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
+import net.minecraftforge.common.data.GlobalLootModifierProvider;
+import net.minecraftforge.common.loot.LootTableIdCondition;
+
+public class ModGlobalLootModifiersProvider extends GlobalLootModifierProvider {
+    public ModGlobalLootModifiersProvider(PackOutput output) {
+        super(output, Konenalamod.MOD_ID);
+    }
+
+    @Override
+    protected void start() {
+       /* add("pine_cone_from_grass", new AddItemModifier(new LootItemCondition[] {
+                LootItemBlockStatePropertyCondition.hasBlockStateProperties(Blocks.GRASS).build(),
+                LootItemRandomChanceCondition.randomChance(0.35f).build()}, ModItems.KIWI.get()));
+*/
+        add("pine_cone_from_creeper", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("entities/creeper")).build() }, ModItems.KIWI.get()));
+
+        add("metal_detector_from_jungle_temples", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/jungle_temple")).build() }, ModItems.METAL_DETECTOR.get()));
+
+
+    }
+}
